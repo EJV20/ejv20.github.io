d45876d68f9323eb8b3402eab72158e4c985db39
Jeff Vitunac <ejv20@pitt.edu>
2019-06-08 18:25:05 -0400

    loop

--- app.js
+++ app.js
@@ -1,15 +1,19 @@
 window.onload = mobile_desktop;
 
 function isMobileDevice() {
+  console.log("ON LOAD WOKRED");
   return (typeof window.orientation !== "undefined") || (navigator.userAgent.indexOf('IEMobile') !== -1);
 };
 
 function mobile_desktop() {
+  console.log("Before if");
   if (isMobileDevice()) {
-    window.alert("MOBILE!")
+    console.log("MOBILE");
     document.getElementsByClassName("main-col").style.width = "";
     document.getElementsByClassName("sidebar").style.width = "";
     document.getElementsByClassName("sidebar").style.float = "";
+  } else {
+    console.log("DESKTOP");
   }
 }
 
