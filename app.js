ad0c9ba93e20300611d5f789ab7fbacd17731239
Jeff Vitunac <ejv20@pitt.edu>
2019-06-08 18:13:23 -0400

    mobile and titles updated
    lit

--- app.js
+++ app.js
@@ -1,14 +1,14 @@
-'use strict';
+window.onload = mobile_desktop;
 
-// Declare app level module which depends on views, and core components
-angular.module('myApp', [
-  'ngRoute',
-  'myApp.view1',
-  'myApp.view2',
-  'myApp.version'
-]).
-config(['$locationProvider', '$routeProvider', function($locationProvider, $routeProvider) {
-  $locationProvider.hashPrefix('!');
+function isMobileDevice() {
+  return (typeof window.orientation !== "undefined") || (navigator.userAgent.indexOf('IEMobile') !== -1);
+};
 
-  $routeProvider.otherwise({redirectTo: '/view1'});
-}]);
+function mobile_desktop() {
+  if (isMobileDevice()) {
+    document.getElementsByClassName("main-col").style.width = "100%";
+    document.getElementsByClassName("sidebar").style.width = "100%";
+    document.getElementsByClassName("sidebar").style.float = "left";
+  }
+}
+
