07259150eecb2cf83d5f7523de95e0ae6e4b48a5
Jeff Vitunac <ejv20@pitt.edu>
2019-06-10 01:57:11 -0400

    yoyoy

--- app.js
+++ app.js
@@ -5,16 +5,28 @@ function isMobileDevice() {
   return (typeof window.orientation !== "undefined") || (navigator.userAgent.indexOf('IEMobile') !== -1);
 };
 
+function isIndex() {
+  let pn = window.location.pathname;
+  console.log(pn);
+  let fs = pn.indexOf("index.html");
+  if ( fs === -1)
+    return 0;
+  else
+    return 1;
+}
+
 function mobile_desktop() {
   let root = document.documentElement;
-  console.log("Before if");
   if (isMobileDevice()) {
-    console.log("mobile by id");
-    let art = document.getElementById('main-col');
-    let asi = document.getElementById('sidebar');
-    art.style.width = '100%';
-    asi.style.width = '100%';
-    asi.style.float = 'left';
+    if (isIndex()) {
+      root.style.setProperty("--box-width", "100%");
+    } else {
+      let art = document.getElementById('main-col');
+      let asi = document.getElementById('sidebar');
+      art.style.width = '100%';
+      asi.style.width = '100%';
+      asi.style.float = 'left';
+    }
   }
 }
 
